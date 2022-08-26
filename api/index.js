75ca4d4a292ef34ef633a65f42768929ebbed214
damian sola <damiansola99@gmail.com>
2022-08-26 17:56:09 -0300

    preparando deploy

--- api/index.js
+++ api/index.js
@@ -25,8 +25,7 @@ const cors = require("cors")
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.use(cors())
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
--- api/src/app.js
+++ api/src/app.js
@@ -5,6 +5,9 @@ const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const cors = require("cors")
 require('./db.js');
+const {
+  URL_FRONT
+} = process.env;
 
 const server = express();
 
@@ -16,7 +19,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use(cors())
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3002'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', URL_FRONT); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
